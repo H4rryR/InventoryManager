f237748b87473cd483dbb4c5b6b03ad9931f103f
Harry Radford <harry.radford@live.co.uk>
2016-08-01 12:17:56 +0100

    Tried to configure the Main, Controller and SignUp classes.

--- InventoryManager/src/application/Controller.java
+++ InventoryManager/src/application/Controller.java
@@ -1,80 +1,27 @@
 package application;
 
-import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class Controller {
-
-	@FXML
-	private TextField tfUsername;
-	@FXML
-	private TextField pfPassword;
-	@FXML
-	private Button btnSignUp;
-	@FXML
-	private Label lblOtherAlert;
-
-	Main main = new Main();
-
-	public void signUpPressed() {
-		System.out.println("Sign Up was pressed");
-		main.username = tfUsername.getText();
-		main.password = pfPassword.getText();
-		
-		if (main.username.length() >= 5 && main.password.length() >= 8) {
-			main.usernames.add(main.username);
-			main.passwords.add(main.password);
-			lblOtherAlert.setText("Account created successfully");
-			
-		} else if (main.username.length() >= 5) {
-			lblOtherAlert.setText("PASSWORD MUST BE 8 CHARACTERS OR LONGER");
-			
-		} else if (main.password.length() >= 8) {
-			lblOtherAlert.setText("USERNAME MUST BE 5 CHARACTERS OR LONGER");
-			
-		} else if (main.username.length() < 5 && main.password.length() < 8) {
-			lblOtherAlert.setText("USERNAME MUST BE 5 CHARACTERS OR LONGER AND PASSWORD MUST BE 8 CHARACTERS OR LONGER");
-			
-		} else {
-			lblOtherAlert.setText("USERNAME MUST BE 5 CHARACTERS OR LONGER AND PASSWORD MUST BE 8 CHARACTERS OR LONGER");
-		}
-		
-		System.out.println("Username: " + main.usernames);
-		System.out.println("Password: " + main.passwords);
-	}
-
-//	public void usernameEnterPressed() {
-//		
-//	}
-//	
-//	public void passwordEnterPressed() {
-//		
-//	}
 	
-	public void signInPressed() {
-		System.out.println("Sign In was pressed");
-	}
-
-	public void signOutPressed() {
-		System.out.println("Sign Out was pressed");
-	}
-
-	public void accountSettingsPressed() {
-		System.out.println("Account Settings was pressed");
-	}
-
-	public void searchPressed() {
-		System.out.println("Search was pressed");
-	}
-
-	public void clearPressed() {
-		System.out.println("Clear was pressed");
-	}
-
-	public void submitPressed() {
-		System.out.println("Submit was pressed");
+	@FXML
+	public TextField tfUsername;
+	@FXML
+	public PasswordField pfPassword;
+	@FXML
+	public Button btnSignUp;
+	@FXML
+	public Label lblOtherAlert;
+	
+	SignUp su = new SignUp(); 
+	
+	public void signUpPressed() {
+		System.out.println("signUpPressed was called");
+		su.signUpValidation();
 	}
 
 }
--- InventoryManager/src/application/SignUp.java
+++ InventoryManager/src/application/SignUp.java
@@ -1,7 +1,43 @@
 package application;
 
-public class SignUp {
+import java.util.ArrayList;
 
+public class SignUp {
 	
+	public String username;
+	public String password;
+	public ArrayList<String> usernames = new ArrayList<String>();
+	public ArrayList<String> passwords = new ArrayList<String>();
+	
+	Controller c = new Controller();
+	
+	public void signUpValidation() {
+	
+	System.out.println("signUpValidation was called");
+	username = c.tfUsername.getText();
+	password = c.pfPassword.getText();
+	
+	if (username.length() >= 5 && password.length() >= 8) {
+		usernames.add(username);
+		passwords.add(password);
+		c.lblOtherAlert.setText("Account created successfully");
+		
+	} else if (username.length() >= 5) {
+		c.lblOtherAlert.setText("PASSWORD MUST BE 8 CHARACTERS OR LONGER");
+		
+	} else if (password.length() >= 8) {
+		c.lblOtherAlert.setText("USERNAME MUST BE 5 CHARACTERS OR LONGER");
+		
+	} else if (username.length() < 5 && password.length() < 8) {
+		c.lblOtherAlert.setText("USERNAME MUST BE 5 CHARACTERS OR LONGER AND PASSWORD MUST BE 8 CHARACTERS OR LONGER");
+		
+	} else {
+		c.lblOtherAlert.setText("USERNAME MUST BE 5 CHARACTERS OR LONGER AND PASSWORD MUST BE 8 CHARACTERS OR LONGER");
+	}
+	
+	System.out.println("Username: " + usernames);
+	System.out.println("Password: " + passwords);
+	
+	}
 	
 }
