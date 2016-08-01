8777d9631837aa4b6b80e621efed3c094d1f776b
Harry Radford <harry.radford@live.co.uk>
2016-08-01 12:23:32 +0100

    Removed the failed attempt to configure the classes.

--- InventoryManager/src/application/Controller.java
+++ InventoryManager/src/application/Controller.java
@@ -1,27 +1,48 @@
 package application;
 
-import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
 
 public class Controller {
-	
+
 	@FXML
-	public TextField tfUsername;
+	private TextField tfUsername;
 	@FXML
-	public PasswordField pfPassword;
+	private TextField pfPassword;
 	@FXML
-	public Button btnSignUp;
+	private Button btnSignUp;
 	@FXML
-	public Label lblOtherAlert;
-	
-	SignUp su = new SignUp(); 
-	
+	private Label lblOtherAlert;
+
+	Main main = new Main();
+
 	public void signUpPressed() {
-		System.out.println("signUpPressed was called");
-		su.signUpValidation();
+		System.out.println("Sign Up was pressed");
+		main.username = tfUsername.getText();
+		main.password = pfPassword.getText();
+		
+		if (main.username.length() >= 5 && main.password.length() >= 8) {
+			main.usernames.add(main.username);
+			main.passwords.add(main.password);
+			lblOtherAlert.setText("Account created successfully");
+			
+		} else if (main.username.length() >= 5) {
+			lblOtherAlert.setText("PASSWORD MUST BE 8 CHARACTERS OR LONGER");
+			
+		} else if (main.password.length() >= 8) {
+			lblOtherAlert.setText("USERNAME MUST BE 5 CHARACTERS OR LONGER");
+			
+		} else if (main.username.length() < 5 && main.password.length() < 8) {
+			lblOtherAlert.setText("USERNAME MUST BE 5 CHARACTERS OR LONGER AND PASSWORD MUST BE 8 CHARACTERS OR LONGER");
+			
+		} else {
+			lblOtherAlert.setText("USERNAME MUST BE 5 CHARACTERS OR LONGER AND PASSWORD MUST BE 8 CHARACTERS OR LONGER");
+		}
+		
+		System.out.println("Username: " + main.usernames);
+		System.out.println("Password: " + main.passwords);
 	}
 
 }
